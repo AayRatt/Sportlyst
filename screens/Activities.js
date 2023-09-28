9bf4c7a1ec9b773efa23d40a4c3006cdc5775cd7
Melo <rommmel@hotmail.com>
2023-09-28 07:51:57 -0400

    User Creation & Location

--- screens/Activities.js
+++ screens/Activities.js
@@ -1,7 +1,44 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
+import { useState, useEffect } from 'react';
+
+// Import Location's Library
+import * as Location from 'expo-location'
 
 export default function Activities({ navigation }) {
+  ///Variables
+  //Location State
+  const [deviceLocation, setDeviceLocation] = useState(null);
+
+
+  // Current Location 
+  const getCurrentLocation = async () => {
+    try {
+      //1. Permissions
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        alert('Permission not granted')
+        return
+        }
+        console.log('Permission granted');
+      //2. Permission Accepted
+      let location = await Location.getCurrentPositionAsync()
+      console.log('Current location is:')
+      console.log(location)
+      // alert(JSON.stringify(location.coords.altitude, location.coords.longitude))
+      setDeviceLocation({lat: location.coords.latitude, lng: location.coords.longitude})
+      console.log('User location:', deviceLocation.lat, deviceLocation.lng);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  //Use effect State
+  useEffect ( () => {
+    getCurrentLocation()
+  },[])
+
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Activities</Text>
