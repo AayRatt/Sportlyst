cbc6f7a61ee2ce7a97c9eb29a52cc74c486b9a37
Nadun perera <nadunnavishka@gmail.com>
2023-11-29 17:00:56 -0500

    Modified android files to fix failed expo build

--- android/app/src/main/java/com/sportlystapp/sportlyst/MainActivity.java
+++ android/app/src/main/java/com/sportlystapp/sportlyst/MainActivity.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.sportlyst.BuildConfig;
+import com.sportlyst.R;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
