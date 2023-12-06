222f6962328a82eda48d79565d9f68c96d0034de
Aayush Rattan <aayush.rattan@ymail.com>
2023-12-06 10:44:41 -0500

    ActivityCard Update

--- components/MyActivityCard.js
+++ components/MyActivityCard.js
@@ -26,42 +26,44 @@ export default function ActivityCard(props) {
   };
 
   return (
-    <View className="shadow-lg">
-      <View className="bg-primary rounded-lg m-5">
-        <View className="flex-row">
-          <Image className="w-full h-40 rounded-t-lg" source={{uri: props.activityImage}} />
-        </View>
-        <View className="px-3 py-3">
-          <View className="flex-row justify-between items-center">
-            <Text className="font-urbanist text-3xl">{props.title}</Text>
-            <View className="flex-row items-center">
-              <Ionicons name="location-sharp" size={24} color="gray" />
-              <View className="w-28">
-                <Text numberOfLines={1} className="font-urbanist text-[#777]">
-                  {props.location}
+    <Pressable onPress={onButtonClicked}>
+      <View className="shadow-lg">
+        <View className="bg-primary m-5 rounded-lg">
+          <View className="flex-row">
+            <Image
+              className="w-full h-52 rounded-t-lg"
+              // source={props.img}
+              source={{ uri: props.activityImage }}
+            ></Image>
+          </View>
+          <View className="px-3 py-3">
+            <View className="flex-row justify-between items-center">
+              <Text className="font-urbanist text-3xl">{props.title}</Text>
+              <View className="flex-row items-center">
+                <Ionicons name="location-sharp" size={24} color="gray" />
+                <View className="w-28">
+                  <Text numberOfLines={1} className="font-urbanist text-[#777]">
+                    {props.location}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View className="flex-row justify-between">
+              <View className="flex-row">
+                <Text className="text-lg font-semibold">$</Text>
+                <Text className="text-lg font-semibold">{props.price}</Text>
+              </View>
+              <View className="flex-row items-center">
+                <Ionicons name="time-outline" size={24} color="gray" />
+                <Text className="font-urbanist text-[#777]">
+                  {props.time} {props.date}
                 </Text>
               </View>
             </View>
-          </View>
-          <View className="flex-row justify-between">
-            <Text className="text-gray-600">Details</Text>
-            <View className="flex-row items-center">
-              <Ionicons name="time-outline" size={24} color="gray" />
-              <Text className="font-urbanist text-[#777]">
-                {props.time} {props.date}
-              </Text>
-            </View>
-          </View>
-          <View className="w-full">
-            <Pressable
-              className="bg-gray rounded-lg w-1/5 h-10 mt-5 items-center justify-center"
-              onPress={onButtonClicked}
-            >
-              <Text className="text-lg font-urbanistBold">View</Text>
-            </Pressable>
+            <View className="w-full"></View>
           </View>
         </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
